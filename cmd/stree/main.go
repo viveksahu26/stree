55a521620b47958c6ebed4b33cd8eb6824b847be
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2025-08-16 11:16:44 +0530

    integrate fang

--- cmd/stree/main.go
+++ cmd/stree/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"log"
+	"context"
+	"os"
 
+	"github.com/charmbracelet/fang"
 	"github.com/viveksahu26/stree/cmd/stree/cli"
 )
 
 func main() {
-	if err := cli.New().Execute(); err != nil {
-		// we don't call os.Exit as Fatalf does both PrintF and os.Exit(1)
-		log.Fatalf("error during command execution: %v", err)
+	// if err := cli.New().Execute(); err != nil {
+	// 	// we don't call os.Exit as Fatalf does both PrintF and os.Exit(1)
+	// 	log.Fatalf("error during command execution: %v", err)
+	// }
+	if err := fang.Execute(
+		context.Background(),
+		cli.New(),
+	); err != nil {
+		os.Exit(1)
 	}
 }
